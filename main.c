304198cac150206173be6f8e254d1a66fff79339
Ekrem Solmaz <ekremsolmazz@gmail.com>
2018-05-29 04:30:53 +0300

    Project completed except some little details
    Reads operations from file.
    Solves  them one by one after converting to postfix form.
    Can use variables from previous operations.

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ typedef struct STACK
 
 
 
-//Char deðerleri icin stack fonlsiyonlari
+//Char deÄŸerleri icin stack fonlsiyonlari
 
 void Initialize(STACK *s)
 {
@@ -75,7 +75,7 @@ char WhatNext(STACK *s)
 }
 
 
-// Integer deðerleri icin stack fonsiyonlari
+// Integer deÄŸerleri icin stack fonsiyonlari
 
 int IsEmptyInt(STACK s)
 {
@@ -153,7 +153,9 @@ char * ToPostfix(char infix[])
     int p = 0;
 
     STACK * stack;
+    stack = (STACK*) malloc(sizeof(STACK));
     Initialize(stack);
+
 
     char c = infix[0];
     int i = 0;
@@ -228,26 +230,25 @@ char * ToPostfix(char infix[])
     return postfix;
 }
 
-int SolvePostfix(char postfix[])
+int SolvePostfix(char postfix[], int alphabet[])
 {
 
-    //islemleri yapmak icin kullanacagým yýgýn.
+    //islemleri yapmak icin kullanacagÄ±m yÄ±gÄ±n.
 
     STACK * solveStack;
     solveStack = (STACK*) malloc(sizeof(STACK));
     Initialize(solveStack);
 
-    //postfix ifadede ilerlemek icin kullanacagýz.
+    //postfix ifadede ilerlemek icin kullanacagÄ±z.
     int p = 0;
     char c = postfix[p];
 
     char integer[10];
     int digitCount = 0;
 
-    //islemler icin kullanacagým int degýskenler
+    //islemler icin kullanacagÄ±m int degÄ±skenler
     int x, y;
 
-    printf("X");
 
     while(c != ';'){
 
@@ -263,6 +264,10 @@ int SolvePostfix(char postfix[])
             printf("%d pushed to stack\n", atoi(integer));
             memset(integer, 0, 9);
             digitCount = 0;
+        }
+        else if(isalpha(c)){
+            PushInt(solveStack, alphabet[c - 97]);
+            printf("%c = %d pushed to stack\n", c, alphabet[c-97]);
         }
         else{
             y = PopInt(solveStack);
@@ -311,8 +316,7 @@ int main()
 
 
 
-
-    /*
+/*
     char op[100];
     printf("Operation :\n");
     gets(op);
@@ -323,26 +327,24 @@ int main()
 
     printf("\n");
     puts(op);
-    */
+*/
 
-
-
-
-    /*
+/*
     char op[100] = "22 37 41 + * ;";
 
     int res = SolvePostfix(op);
     printf("\nResult : %d", res);
 
-    */
+*/
+
+/*
+    char op[100]= "13 * ( 13 - 7 ) / 2 ;";
 
 
-    char op[100]= "3 * ( 13 - 7 ) / 2 ;";
-    /*
     printf("Operation :\n");
     gets(op);
     printf("\n");
-    */
+
     puts(op);
 
     strcpy(op, ToPostfix(op));
@@ -353,8 +355,117 @@ int main()
     printf("\nResult : %d", res);
 
 
+*/
+
+/*
+    int alphabet[26] = {0};
+
+
+    //a
+
+    char operation[100] = "a = 3 * ( 5 + 6 ) ;";
+    char rightSide[100];
+    memcpy(rightSide, &operation[4], 95);
+
+    char postfix[100];
+    strcpy(postfix, ToPostfix(rightSide));
+
+    alphabet[operation[0] - 97] = SolvePostfix(postfix, alphabet);
+
+    printf("\n%c = %d", operation[0], alphabet[operation[0] - 97]);
+
+
+    //b
+
+    char operation2[100] = "b = 3 + a * 2 ;";
+    char rightSide2[100];
+    memcpy(rightSide2, &operation2[4], 95);
+
+    char postfix2[100];
+    strcpy(postfix2, ToPostfix(rightSide2));
+
+    alphabet[operation2[0] - 97] = SolvePostfix(postfix2, alphabet);
+
+     printf("\n%c = %d", operation2[0], alphabet[operation2[0]- 97]);
+
+
+*/
+
+
+    int alphabet[26] = {NULL};
+
+
+
+    FILE *fr;
+    fr = fopen("ops.txt", "r");
+
+
+    int i = 0;
+    char line[100], rightSide[100], postfix[100];
+    char c;
+
+    while(fgets(line, 100, fr) != NULL){
+
+
+
+        memcpy(rightSide, &line[4], 95);
+        strcpy(postfix, ToPostfix(rightSide));
+
+        alphabet[line[0] - 97] = SolvePostfix(postfix, alphabet);
+
+        puts(line);
+        puts(rightSide);
+        puts(postfix);
+
+
+        printf("\n%c = %d", line[0], alphabet[line[0]- 97]);
+
+        memset(line, 0, 99);
+        memset(rightSide, 0, 99);
+        memset(postfix, 0, 99);
+        i = 0;
+
+
+        printf("\n-------------------------\n");
+
+
+    }
+
+
+    printf("RESULTS : \n");
+
+    for(i = 0 ; i < 26 ; i++){
+
+        if(alphabet[i] != NULL){
+
+            printf("%c = %d\n", (char)(i+97) , alphabet[i]);
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
 
 
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
